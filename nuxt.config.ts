633606d932db4d8e5192ba2e71e962b8c94724b1
katrineperez <katrine@pollen.tech>
2024-08-21 16:28:44 +0800

    fix: add github action to sandbox s3

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,31 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'Pollen Direct',
+      title: "Pollen Direct",
       htmlAttrs: {
-        lang: 'en',
+        lang: "en",
       },
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-      meta: [{ name: 'robots', content: 'noindex' }],
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      meta: [{ name: "robots", content: "noindex" }],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/icon.png' },
+        { rel: "icon", type: "image/x-icon", href: "/icon.png" },
         {
-          rel: 'stylesheet',
-          href: 'https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap',
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/css2?family=Poppins:wght@200;400&display=swap",
         },
       ],
     },
   },
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: [
-    '@nuxt/eslint',
-    '@pinia/nuxt',
+    "@nuxt/eslint",
+    "@pinia/nuxt",
     (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
+      nuxt.hooks.hook("vite:extendConfig", (config) => {
         // @ts-expect-error: Adding Vuetify plugin to Vite config despite TypeScript type issues
         config.plugins.push(vuetify({ autoImport: true }));
       });
@@ -33,9 +33,9 @@ export default defineNuxtConfig({
     //...
   ],
   build: {
-    transpile: ['vuetify'],
+    transpile: ["vuetify"],
   },
-  css: ['@/assets/scss/style.scss'],
+  css: ["@/assets/scss/style.scss"],
   vite: {
     vue: {
       template: {
@@ -48,14 +48,13 @@ export default defineNuxtConfig({
     public: {
       // pollenPassUrl:
       //   "http://web.pollen-pass.s3-website-ap-southeast-1.amazonaws.com", // "http://sandbox.web.pollen-pass.s3-website-ap-southeast-1.amazonaws.com",
-      pollenPassUrl: 'https://pollen-pass-sb.pollentech.cloud/',
-      pollenLMSUrl: 'https://lms-sb.pollentech.cloud', //"http://sandbox.web.lms.s3-website-ap-southeast-1.amazonaws.com",
-      lmsBackendUrl: 'https://helix-sb.pollentech.cloud/liquidate/api/v1/', // https://47ekwsnw5d.execute-api.ap-southeast-1.amazonaws.com/dev/
-      directBackendUrl: 'https://helix-sb.pollentech.cloud/direct/api/v1/',
-      userOnboardBackendUrl:
-        'https://helix-sb.pollentech.cloud/user-onboard/api/v1/',
-      appName: 'Pollen LMS',
-      appVersion: '2.0.0',
+      pollenPassUrl: process.env.NUXT_POLLEN_PASS_URL,
+      pollenLMSUrl: process.env.NUXT_POLLEN_LMS_URL, //"http://sandbox.web.lms.s3-website-ap-southeast-1.amazonaws.com",
+      lmsBackendUrl: "https://helix-sb.pollentech.cloud/liquidate/api/v1/", // https://47ekwsnw5d.execute-api.ap-southeast-1.amazonaws.com/dev/
+      directBackendUrl: process.env.NUXT_DIRECT_API,
+      userOnboardBackendUrl: process.env.NUXT_LMS_API,
+      appName: "Pollen LMS",
+      appVersion: "2.0.0",
     },
   },
 });
