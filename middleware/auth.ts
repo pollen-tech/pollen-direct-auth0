103f540eba8031afb383fe76671e6150f268471d
katperezz29 <katrine@pollen.tech>
2024-08-27 13:51:34 +0800

    Fix/add deploy production (#10)
    * merge current branch
    
    * fix: add deploy to production on trigger

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,5 +1,5 @@
-import { useAuth } from '~/composables/auth0';
-import { useSellerStore } from '~/stores/seller';
+import { useAuth } from "~/composables/auth0";
+import { useSellerStore } from "~/stores/seller";
 
 export default defineNuxtRouteMiddleware(async (to, _from) => {
   const seller_store = useSellerStore();
@@ -8,25 +8,25 @@ export default defineNuxtRouteMiddleware(async (to, _from) => {
   const user_id = get_user_id();
 
   if (is_token_expired()) {
-    if (typeof window !== 'undefined') {
+    if (typeof window !== "undefined") {
       localStorage.clear();
-      if (to.name === 'onboarding') {
-        return navigateTo('/');
+      if (to.name === "onboarding") {
+        return navigateTo("/");
       }
     }
   } else {
     const { data } = await seller_store.get_company_profile(user_id);
     if (data?.id) {
       const interest = await seller_store.get_company_interest(data?.id);
-      if (to.name === 'onboarding') {
+      if (to.name === "onboarding") {
         if (interest?.data?.import_markets.length >= 1) {
-          window.location.href = '/';
+          window.location.href = "/";
         }
       } else {
-        window.location.href = '/'; // Redirect to home if not authenticated
+        window.location.href = "/"; // Redirect to home if not authenticated
       }
-    } else if (to.name == 'index') {
-      window.location.href = '/onboarding';
+    } else if (to.name == "index") {
+      window.location.href = "/onboarding";
     }
   }
 });
