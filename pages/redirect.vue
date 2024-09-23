a089d362bfa3170c9ab88794c0dd9570d0ba415e
katrineperez <katrine@pollen.tech>
2024-09-23 13:49:41 +0800

    fix: add async to redirect

--- pages/redirect.vue
+++ pages/redirect.vue
@@ -27,35 +27,34 @@ const { get_user_profile_channel } = user_Store;
 
 const common_store = useCommonStore();
 
-onMounted(() => {
-
+onMounted(async () => {
   console.log(route.query);
 
-    if (route.query.user_id) {
-      try {
-        const req = await get_user_profile_channel(route.query);
-        if (req.status_code == "OK") {
-          await auth.handleAuth0Response(route.query);
-          await auth.set_user_id(req.data.user_id);
-          await nextTick();
-          router.push("/onboarding");
-        } else {
-          common_store.setShowNotification({
-            display: true,
-            status: "error",
-            msg: req.desc,
-          });
-          setTimeout(() => {
-            router.push("/");
-          }, 400);
-        }
-      } catch (error) {
-        console.error("Navigation error:", error);
+  if (route.query.user_id) {
+    try {
+      const req = await get_user_profile_channel(route.query);
+      if (req.status_code == "OK") {
+        await auth.handleAuth0Response(route.query);
+        await auth.set_user_id(req.data.user_id);
+        await nextTick();
+        router.push("/onboarding");
+      } else {
+        common_store.setShowNotification({
+          display: true,
+          status: "error",
+          msg: req.desc,
+        });
+        setTimeout(() => {
+          router.push("/");
+        }, 400);
       }
-    } else if (is_authenticated.value) {
-      window.location.href = "/";
-      isLoading.value = false;
+    } catch (error) {
+      console.error("Navigation error:", error);
     }
+  } else if (is_authenticated.value) {
+    window.location.href = "/";
+    isLoading.value = false;
+  }
 });
 </script>
 
